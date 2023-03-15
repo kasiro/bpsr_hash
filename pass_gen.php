745458c1b782f410c346421965f5c188e8f8cf86
kasiro <kon.konikov@mail.ru>
2023-03-16 02:10:15 +0700

    last

--- pass_gen.php
+++ pass_gen.php
@@ -1,18 +1,23 @@
 <?php
 
-// require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 require 'my_hash.php';
+require './jhp_modules/uid_generator.php';
 
-$login = 'kon.konikov@mail.ru';
-$pass = '45105!45105!';
-$site = 'github.com';
+$login = '89137759910';
+$pass = 'truerealyexp';
+$site = 'vk.com';
 $sym = '|';
 
-// $ps_test = $login.$sym.$site.$sym.$pass;
-$ps = $login.$sym.$site.$sym.$pass;
-echo $ps . PHP_EOL;
+uid_generator::$abc = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-// $ps_hash = bpsr_wrapper($ps);
-// echo $ps_hash . PHP_EOL;
-echo $site.': '.password_generate(strlen($ps)) . PHP_EOL;
+// srand();
+$res = uid_generator::uid_generate('xxxx-xxxx-xxxx-xxxx', 'x');
+// $ps_test = $login.$sym.$site.$sym.$pass;
+// $ps = $login.$sym.$site.$sym.$pass;
+// echo $ps . PHP_EOL;
+
+$ps_hash = bpsr_wrapper($res);
+echo $ps_hash . PHP_EOL;
+// echo $site.': '.password_generate(strlen($ps)) . PHP_EOL;
 // dump(bpsr_verify($ps, $ps_hash));
---
+++ pass_test.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'my_hash.php';
+require './jhp_modules/uid_generator.php';
+uid_generator::$abc = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+$hash = trim(file_get_contents('bpsr.hash'));
+$r = (int) explode('$', $hash)[2];
+for ($i = 1; $i <= 999999999999999; $i++) {
+    srand($i);
+    $res = uid_generator::uid_generate('xxxx-xxxx-xxxx-xxxx', 'x');
+    $h = bpsr_wrapper($res, $r);
+    if ($h == $hash) {
+        dd($res);
+    }
+    echo $res . ' (' . $h . ')'. ' (' . $i . ')' . PHP_EOL;
+}
