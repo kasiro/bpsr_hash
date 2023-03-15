516b45e38adb4d58e788f72c3ad967e7b02a6170
kasiro <kon.konikov@mail.ru>
2023-03-16 02:14:27 +0700

    last 2

--- pass_test.php
+++ pass_test.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 require 'my_hash.php';
 require './jhp_modules/uid_generator.php';
+require './jhp_modules/Colors.php';
 uid_generator::$abc = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 $hash = trim(file_get_contents('bpsr.hash'));
@@ -14,5 +15,5 @@ for ($i = 1; $i <= 999999999999999; $i++) {
     if ($h == $hash) {
         dd($res);
     }
-    echo $res . ' (' . $h . ')'. ' (' . $i . ')' . PHP_EOL;
+    echo Colors::setColor($res . ' (' . number_format($i, 0, ' ', ' ') . ')', 'red') . PHP_EOL;
 }
